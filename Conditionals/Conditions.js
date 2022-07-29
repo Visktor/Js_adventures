f0bf368224dc9bef1d835e4b8ca12b4eef651c8e
Vitor <webdev.vitor@gmail.com>
2022-07-29 18:40:52 -0300

    Add some conditionals' examples.

--- Conditionals/Conditions.js
+++ Conditionals/Conditions.js
@@ -42,3 +42,43 @@ let message = (age2 < 3) ? 'Hi, baby!' :
   (age2 < 100) ? 'Greetings!' :
 
   alert( message );
+
+// exercise 01:
+
+let number1 = prompt('What is the number?', '');
+(number1 < 0) ? alert('-1') : (number1 == 0) ? alert('0') : alert('1');
+
+// Interesting application of Switch:
+
+// Get number corresponding to the current month, with 0 being January and 11 being December
+const month = new Date().getMonth();
+
+switch (month) {
+	// January, February, March
+	case 0:
+	case 1:
+	case 2:
+		console.log("Winter");
+		break;
+	// April, May, June
+	case 3:
+	case 4:
+	case 5:
+		console.log("Spring");
+		break;
+	// July, August, September
+	case 6:
+	case 7:
+	case 8:
+		console.log("Summer");
+		break;
+	// October, November, December
+	case 9:
+	case 10:
+	case 11:
+		console.log("Autumn");
+		break;
+	default:
+    console.log("Something went wrong.");
+}
+
