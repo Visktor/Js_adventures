3680e9ad7843fe586c29b0d0ac079560ce96f016
Vitor <webdev.vitor@gmail.com>
2022-07-30 09:46:38 -0300

    Add Functions folder

---
+++ Functions/01/functionindex.js
@@ -0,0 +1,21 @@
+function sum(num, num2){
+    return num + num2;
+}
+
+let test = sum(5, 12);
+
+//Basic function utilizing numbers
+
+console.log(test)
+
+function str(a, b){
+    let strs = `${a} ${b}`;
+    return strs; 
+}
+
+const str1 = 'Well hello there.';
+const str2 = 'May I know your name?';
+
+console.log(str(str1, str2));
+
+//Basic function utilizing Strings.
